c88b1f3c2db80e5fdd2bab208b853aee94209c98
abdillah attoumani <a.attoumani13@outlook.fr>
2022-12-07 22:43:14 +0100

    skip devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters
   before_action :authenticate_user!
 
   def configure_permitted_parameters
